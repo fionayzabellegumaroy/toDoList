afc79d17f7878fdf2e715afe74b58183fa9e71c7
Fiona Yzabelle Gumaroy <126933200+fionayzabellegumaroy@users.noreply.github.com>
2024-08-12 17:35:52 -0400

    Add files via upload

--- code.js
+++ code.js
@@ -1,14 +1,24 @@
 let input = document.getElementById("inputField");
 let button = document.getElementById("add");
-let toDoContainer = document.getElementById("toDoContainer");
+let boxContainer = document.getElementById("boxContainer");
 let monster = document.getElementById("monster");
 let monsterPicture = document.getElementById("monsterPicture");
 let difficultyEasy = document.getElementById("difficultyEasy");
 let difficultyMed = document.getElementById("difficultyMed");
 let difficultyHard = document.getElementById("difficultyHard");
 let initialImg = document.getElementById("questionMark");
+let container = document.getElementById("container");
+let instructionsButton = document.getElementById("instructionsButton");
+let errorRateContainer = document.getElementById("errorRateContainer");
+let errorInputContainer = document.getElementById("errorInputContainer")
+let instructionsContainer = document.getElementById("instructionsContainer");
+let rateXButton = document.getElementById("rateXButton");
+let inputXButton = document.getElementById("inputXButton");
+let toDoContainer = document.getElementById("toDoContainer");
+let instructionsReopenButton = document.getElementById("instructionsReopenButton");
 
 let monsters = ["lucifer", "badidi", "papi"];
+let radios = [difficultyEasy, difficultyMed, difficultyHard];
 var monsterImg = document.createElement("img");
 
 let randomNum = Math.floor(Math.random() * monsters.length);
@@ -24,45 +34,76 @@ let health = document.getElementById("health");
 let levelNumber = 1;
 let taskCount = 0;
 
+input.style.display = "none";
+button.style.display = "none";
+button.disabled = true;
+
+radios.forEach((radio) => {
+    radio.addEventListener("input", function(e){
+        if (radio.checked == true){
+            input.style.display = "inline-block";
+            button.style.display = "inline-block";
+
+            input.addEventListener("input", function(e){
+                if (input.value.length > 0){          
+                    button.disabled = false;
+                } else {
+                    button.disabled = true;
+                }
+            }) 
+        }
+    })
+})
+
+//closes instruction window
+instructionsButton.addEventListener('click', () =>{
+    instructionsContainer.style.display = "none";
+    boxContainer.style.display = "block";
+})
+
+//reopens instruction window
+instructionsReopenButton.addEventListener('click', () =>{
+    instructionsContainer.style.display = "block";
+    boxContainer.style.display = "none";
+})
+
+
+//task array and class
+var tasksArray = [];
+
+class Task {
+    constructor(task, difficulty) {
+      this._task = task;
+      this._difficulty = difficulty;
+
+    }
+
+    get task() {
+      return this._task;
+    }
+    
+    get difficulty() {
+      return this._difficulty;
+    }
+}
+
+//function to find task in array
+function findTask(tasksArray, actualTask){
+    for (let i = 0; i < tasksArray.length; i++){
+        var task = tasksArray[i];
+        if (task._task == actualTask){ //maybe .equals
+            return task;
+        };
+    }
+}
+  
+
 button.addEventListener('click', () => {
 
-    //makes health visible
-    health.style.opacity = "100";
-    monster.style.opacity = "100";
-
-    //randomize monster
-    monster.innerText = monsters[randomNum];
-
-    // //remove initial picture
-    if (monsterPicture.querySelector(".initialImg")){
-        monsterPicture.removeChild(initialImg);
-    } else if (monsterPicture.querySelector(".monsterImg")){
-        monsterPicture.removeChild(monsterImg);
-    }
-
-    if (randomNum == 0){
-        monsterImg.src = "lucifer.gif";
-        monsterImg.style.backgroundColor = "lightblue";
-    } else if (randomNum == 1){
-        monsterImg.src = "badidi.gif";
-        monsterImg.style.backgroundColor = "lightpink";
-    } else {
-        monsterImg.src = "papi.gif";
-        monsterImg.style.backgroundColor = "lightyellow";
-    }
-
-    //adds style to monsterImg and adds it
-    monsterImg.style.maxHeight = "300px";
-    monsterImg.style.border = "black solid 2px";
-    monsterImg.style.padding = "10px";
-    monsterImg.classList.add("monsterImg");
-    monsterPicture.appendChild(monsterImg);
-
-    //counts number of tasks
     taskCount++;
 
-    //adds task
-    var task = document.createElement("p");
+    //adds initial task!
+    var taskToBeAdded = document.createElement("p");
     var difficulty;
     if (document.getElementById("difficultyEasy").checked){
         difficulty = "easy";
@@ -73,65 +114,133 @@ button.addEventListener('click', () => {
     }
 
     // adds a style to said element
-    task.classList.add("task-styling");
-    task.innerText = `${taskCount}. ${input.value}`;
-    toDoContainer.appendChild(task);
+    taskToBeAdded.classList.add("task-styling");
+    taskToBeAdded.innerText = `${input.value}`;
+    toDoContainer.appendChild(taskToBeAdded);
 
-    //removes input
+    //creates a task object to add to the array
+    var taskObject = new Task(taskToBeAdded.innerText, difficulty);
+    tasksArray.push(taskObject);
+
+    //resets inputs
+    button.disabled = "true"; 
+    input.style.display = "none";
+    button.style.display = "none";
     input.value = "";
     difficultyEasy.checked = false;
     difficultyMed.checked = false;
-    difficultyHard.checked = false;
+    difficultyHard.checked = false;   
+ 
+    //makes health visible
+    health.style.opacity = "100";
+    monster.style.opacity = "100";
+    
+    //randomize monster
+    monster.innerText = monsters[randomNum];
 
-  
+    // //remove initial picture
+    if (monsterPicture.querySelector(".initialImg")){
+        monsterPicture.removeChild(initialImg);
+    } else if (monsterPicture.querySelector(".monsterImg")){
+        monsterPicture.removeChild(monsterImg);
+    }
 
-    task.addEventListener('click', () => {
-        if (task.style.textDecoration == "line-through"){
-            task.style.textDecoration = "none";
+    //monsterPicture
+        if (randomNum == 0){
+            monsterImg.src = "lucifer.gif";
+            monsterImg.style.backgroundColor = "lightblue";
+        } else if (randomNum == 1){
+            monsterImg.src = "badidi.gif";
+            monsterImg.style.backgroundColor = "lightpink";
         } else {
-            task.style.textDecoration = "line-through";
+            monsterImg.src = "papi.gif";
+            monsterImg.style.backgroundColor = "lightyellow";
+        }
+
+        //adds style to monsterImg and adds it
+        monsterImg.style.maxHeight = "300px";
+        monsterImg.style.border = "black solid 2px";
+        monsterImg.style.padding = "10px";
+        monsterImg.classList.add("monsterImg");
+        monsterPicture.appendChild(monsterImg);
+
+    //xbutton click
+    rateXButton.addEventListener('click', ()=> {
+        errorRateContainer.style.display = "none";
+    })
+
+    inputXButton.addEventListener('click', ()=> {
+        errorInputContainer.style.display = "none";
+    })
+
+    radios.forEach((radio) => {
+        radio.addEventListener("input", function(e){
+            if (radio.checked == true){
+                input.style.display = "inline-block";
+                button.style.display = "inline-block";
+    
+                input.addEventListener("input", function(e){
+                    if (input.value.length > 0){          
+                        button.disabled = false;
+                    } else {
+                        button.disabled = true;
+                    }
+                }) 
+            }
+        })
+    })
+   
+    
+    
+    taskToBeAdded.addEventListener('click', () => {
+        if (taskToBeAdded.style.textDecoration == "line-through"){
+            taskToBeAdded.style.textDecoration = "none";
+        } else {
+            taskToBeAdded.style.textDecoration = "line-through";
         }
     })
 
-    task.addEventListener("dblclick", () =>{
-        toDoContainer.removeChild(task);
-        if (difficulty === "easy"){
+    taskToBeAdded.addEventListener("dblclick", (event) =>{
+            
+        var taskInQuestion = findTask(tasksArray, event.target.innerText); //idk about task.value!!
+        
+        tasksArray.pop(taskInQuestion);
+        toDoContainer.removeChild(taskToBeAdded);
+            
+        if (taskInQuestion._difficulty === "easy"){
             health.value -= 1*levelNumber;
         } 
-        else if (difficulty === "med"){
+        else if (taskInQuestion._difficulty === "med"){
             health.value -= 2*levelNumber;
         } 
         else {
             health.value -= 3*levelNumber;
         } 
+                
+            //decreases task count
+            taskCount--;
+    
+            if (health.value <= 0){
+                monsterImg.src = "questionMark.gif";
+                monsterImg.style.backgroundColor = "lightyellow";
+    
+                maxNumber += maxNumber*3;
+                health.max = maxNumber;
+                health.value = maxNumber;
+                levelNumber += 1;
+    
+    
+                //randomize monster
+                randomNum = Math.floor(Math.random() * monsters.length);
+                monster.innerText = monsters[randomNum];
+    
+                health.style.opacity = "0";
+                monster.style.opacity = "0";
 
-        //decreases task count
-        taskCount--;
-
-        if (health.value <= 0){
-            monsterImg.src = "questionMark.gif";
-            monsterImg.style.backgroundColor = "lightyellow";
-
-            maxNumber += maxNumber*3;
-            health.max = maxNumber;
-            health.value = maxNumber;
-            levelNumber += 1;
-
-
-            //randomize monster
-            randomNum = Math.floor(Math.random() * monsters.length);
-            monster.innerText = monsters[randomNum];
-
-            health.style.opacity = "0";
-            monster.style.opacity = "0";
-
-        }
+            }
     })
-
-    // level.innerText = `level ${levelNumber}`;
-
 })
 
+//maybe do an animation once monster dies!
 
-
-//create a function for if a radio button is not checked, show an error
+//create an instructions button to reopen
